b0f729a1f05ded7e3dc947bad982c4be98c756d1
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-10-14 15:27:33 -0700

    [build] Added helper script for development

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Build the dev image if it does not exist already
+if ! docker inspect strace-dev &> /dev/null ; then
+    docker build -t strace-dev .
+fi
+
+docker run --rm -v $PWD:/strace -w /strace strace-dev make
