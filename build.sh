426411634db084673972c0abb37c140b4d12bd31
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-10-17 16:12:45 -0700

    [:whale:] Made a development and a production dockerfile

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Build the dev image if it does not exist already
 if ! docker inspect strace-dev &> /dev/null ; then
-    docker build -t strace-dev .
+    docker build -t strace-dev -f Dockerfile.dev .
 fi
 
 docker run --rm -v $PWD:/strace -w /strace strace-dev make
