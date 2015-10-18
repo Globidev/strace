3f46a71c886e0490f4e0a0e15505c682b0f52661
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-10-18 21:53:39 +0200

    Added more error messages

--- includes/error.h
+++ includes/error.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#define ERR_FORK "Fork failed"
-#define ERR_EXEC "Exec failed"
-#define ERR_STAT "Stat failed"
+#define ERR_FORK "fork failed"
+#define ERR_EXEC "exec failed"
+#define ERR_STAT "stat failed"
+#define ERR_WAITPID "waitpid failed"
+
+#define ERR_PTRACE_SEIZE "PTRACE_SEIZE failed"
+#define ERR_PTRACE_PEEKUSER "PTRACE_PEEKUSER failed"
 
 void fatal(const char *error);
