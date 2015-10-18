dbcae6978788f88dc97a0d4e46763844191f0f4b
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-10-18 10:09:08 -0700

    Added path resolution utilities

---
+++ includes/path.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define ENV_PATH "PATH"
+#define ENV_PATH_SEPARATOR ":"
+
+char *resolve_program_path(const char *program);
---
+++ srcs/path.c
@@ -0,0 +1,54 @@
+#define _GNU_SOURCE /* Enabling asprintf */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "path.h"
+
+static int should_skip_path_search(const char *program)
+{
+    int is_relative;
+    int is_absolute;
+
+    is_relative = (program[0] == '.');
+    is_absolute = (program[0] == '/');
+
+    return (is_relative || is_absolute);
+}
+
+static char *find_in_path(const char *program, char *path)
+{
+    char *prefix;
+    char *concatenated;
+
+    prefix = strtok(path, ENV_PATH_SEPARATOR);
+    while (prefix) {
+        asprintf(&concatenated, "%s/%s", prefix, program); /* TODO: normalize */
+        if (access(concatenated, F_OK) == 0)
+            return (concatenated);
+        else
+            free(concatenated);
+        prefix = strtok(NULL, ENV_PATH_SEPARATOR);
+    }
+
+    return (NULL);
+}
+
+char *resolve_program_path(const char *program)
+{
+    char *env_path;
+    char *path;
+
+    if (should_skip_path_search(program))
+        path = strdup(program);
+    else {
+        env_path = getenv(ENV_PATH);
+        if (!env_path) /* No path variable */
+            path = strdup(program);
+        else
+            path = find_in_path(program, env_path);
+    }
+
+    return (path);
+}
