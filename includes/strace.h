e933cdc1f9e42186d3daa037efc2cb3b8af4064e
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-10-18 21:54:01 +0200

    Added some more defines

--- includes/strace.h
+++ includes/strace.h
@@ -2,4 +2,9 @@
 
 #define PROGRAM_NAME "ft_strace"
 
+#define PTRACE_OPTIONS PTRACE_O_TRACESYSGOOD
+#define SYSCALL_TRAP_MASK 0x80
+
+#define EXIT_MESSAGE "+++ exited with %d +++\n"
+
 int strace(char **command);
