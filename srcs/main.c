40d8982e0df7498d583b3c3ebe9f12ec30c7551e
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-10-18 07:06:10 -0700

    Added a usage

--- srcs/main.c
+++ srcs/main.c
@@ -1,14 +1,23 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "strace.h"
+
+static void show_usage()
+{
+    printf("usage: %s PROG [ARGS]\n", PROGRAM_NAME);
+}
 
 int main(int argc, char **argv)
 {
     int exit_code;
 
-    if (argc >= 2) {
+    if (argc < 2) {
+        show_usage();
+        return (EXIT_FAILURE);
+    }
+    else {
         exit_code = strace(argv + 1);
         return (exit_code);
     }
-    return (EXIT_FAILURE);
 }
