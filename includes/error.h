eb6c04e3f4b255c04b80071a4ee655a2e174ea89
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-10-18 10:08:53 -0700

    Added a fatal helper

--- includes/error.h
+++ includes/error.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#define ERR_FORK "Fork failed"
+#define ERR_EXEC "Exec failed"
+#define ERR_STAT "Stat failed"
+
 void fatal(const char *error);
--- srcs/error.c
+++ srcs/error.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 #include "strace.h"
 
-void fatal(const char *prefix)
+void fatal(const char *description)
 {
-    perror(prefix);
+    fprintf(
+        stderr,
+        "%s: %s: %s\n",
+        PROGRAM_NAME,
+        description,
+        strerror(errno)
+    );
     exit(EXIT_FAILURE);
 }
