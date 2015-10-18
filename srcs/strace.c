ad5b15060e0f00a95f5db20378490a88c812402b
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-10-18 21:54:49 +0200

    Refactored the main tracing code

--- srcs/strace.c
+++ srcs/strace.c
@@ -5,46 +5,83 @@
 #include <sys/wait.h>
 
 #include <sys/ptrace.h>
-#include <sys/reg.h>
 
+#include "strace.h"
+#include "path.h"
+#include "log.h"
 #include "error.h"
 
 extern char **environ;
 
+static int wait_for_syscall_trap(pid_t pid, int *status)
+{
+    int sig_num;
+
+    for (;;) {
+        (void)ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+        if (waitpid(pid, status, 0) == -1)
+            return (1);
+
+        if (WIFSTOPPED(*status)) {
+            sig_num = WSTOPSIG(*status);
+            if (sig_num & SYSCALL_TRAP_MASK)
+                return (0);
+        }
+        else if (WIFEXITED(status))
+            return (1);
+    }
+}
+
+int trace_process(pid_t pid)
+{
+    int last_status;
+    int exit_code;
+
+    /* Become a tracer */
+    if (ptrace(PTRACE_SEIZE, pid, NULL, PTRACE_OPTIONS))
+        fatal(ERR_PTRACE_SEIZE);
+    /* Trace until exited */
+    for (;;) {
+        /* First trap: before effectively executing the syscall */
+        if (wait_for_syscall_trap(pid, &last_status))
+            break ;
+        else
+            show_syscall_invocation(pid);
+        /* Second trap: after the syscall has been executed */
+        if (wait_for_syscall_trap(pid, &last_status)) {
+            fprintf(stderr, " = ?\n"); /* Too late to peek the return value */
+            break ;
+        }
+        else
+            show_syscall_return_value(pid);
+    }
+
+    exit_code = WEXITSTATUS(last_status);
+    fprintf(stderr, EXIT_MESSAGE, exit_code);
+    return (exit_code);
+}
+
 int strace(char **command)
 {
+    char *program;
     pid_t pid;
+    int exit_code;
+
+    exit_code = 0;
+    program = resolve_program_path(command[0]);
+    if (!program)
+        fatal(ERR_STAT);
 
     pid = fork();
     if (pid == -1)
-        fatal("Fork failed");
+        fatal(ERR_FORK);
     else if (pid == 0) {
-        kill(getpid(), SIGSTOP);
-        execve(command[0], command, environ);
-        fatal("exec failed");
+        kill(getpid(), SIGSTOP); /* Waiting to be traced by the parent */
+        execve(program, command, environ);
+        fatal(ERR_EXEC);
     }
-    else {
-        ptrace(PTRACE_SEIZE, pid, 0, PTRACE_O_TRACESYSGOOD);
-        int i = 0;
-        for(;;) {
-            
-            for (;;) {
-                ptrace(PTRACE_SYSCALL, pid, 0, 0);
-                int status;
-                waitpid(pid, &status, 0);
-                if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80)
-                    break ;
-                else if (WIFEXITED(status))
-                    return(0);
-            }
+    else
+        exit_code = trace_process(pid);
 
-            int syscall = ptrace(
-                PTRACE_PEEKUSER,
-                pid,
-                sizeof(long)* (i++ % 2 ? RAX : ORIG_RAX)
-            );
-            printf(i % 2 ? "syscall(%d) = " : "%x\n", syscall);
-        }
-    }
-    return (0);
+    return (exit_code);
 }
