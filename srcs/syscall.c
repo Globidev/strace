ec37bb95ba51f12f89780b4aad91532fdb02b633
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-10-24 03:32:31 +0200

    Fixed write's argument types

--- srcs/syscall.c
+++ srcs/syscall.c
@@ -4,7 +4,7 @@
 
 const syscall_info SYSCALLS[] = {
     { "read",  long_, 3, { int_,    pointer_, long_ } },
-    { "write", long_, 3, { int_,    pointer_, long_ } },
+    { "write", long_, 3, { int_,    string_,  long_ } },
     { "open",  int_,  2, { string_, int_            } },
     { "close", int_,  1, { int_                     } },
 };
