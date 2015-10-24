82ac5e9328af703a8ee816a3f18e964a3746c1ce
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-10-24 03:33:20 +0200

    Refactored invocation logging

--- srcs/log.c
+++ srcs/log.c
@@ -1,37 +1,76 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include <string.h>
 #include <errno.h>
 
 #include "error.h"
+#include "peek.h"
 #include "syscall.h"
+
+#define MAX_LINE_SIZE 256
+#define MIN_PADDING 40
+#define PLACEHOLDER_TEXT "Unknown syscall"
+
+static void str_write(char *str, int *written, const char *format, ...)
+{
+    va_list args;
+
+    va_start(args, format);
+    if (*written < MAX_LINE_SIZE) {
+        *written += vsnprintf(
+            str + *written,
+            MAX_LINE_SIZE - *written,
+            format,
+            args
+        );
+    }
+    va_end(args);
+}
 
 void output_invocation(long syscall_id, syscall_arg *args)
 {
-    char invocation_str[256];
+    char str[MAX_LINE_SIZE];
     const syscall_info *info;
     int written;
     unsigned i;
 
     info = get_syscall_info(syscall_id);
     if (info) {
-        written = 0;
-        written = snprintf(invocation_str, 256, "%s(", info->name);
+        written = snprintf(str, MAX_LINE_SIZE, "%s(", info->name);
         for (i = 0; i < info->arg_count; ++i) {
             switch (info->args_type[i]) {
-                case int_:     written += snprintf(invocation_str + written, 256 - written, "%d, ", (int)(long)args[i]); break;
-                case long_:    written += snprintf(invocation_str + written, 256 - written, "%ld, ",(long)args[i]); break;
-                case pointer_: written += snprintf(invocation_str + written, 256 - written, "%p, ", (void *)args[i]); break;
-                case string_:  written += snprintf(invocation_str + written, 256 - written, "%s, ", (char *)args[i]); break; // TODO
-                case array_:   written += snprintf(invocation_str + written, 256 - written, "[...]"); break; // TODO
+                case int_:
+                    str_write(str, &written, "%d, ", (int)(long)args[i]);
+                    break ;
+                case long_:
+                    str_write(str, &written, "%ld, ", (long)args[i]);
+                    break ;
+                case pointer_:
+                    str_write(str, &written, "%p, ", args[i]);
+                    break;
+                case string_:
+                    str_write(
+                        str,
+                        &written,
+                        "\"%s\"%s, ",
+                        (char *)args[i], // TODO: escape
+                        ((char *)args[i])[STRING_PEEK_MAX_SIZE - 1] ? "..." : ""
+                    );
+                    break;
+                case array_:
+                    str_write(str, &written, "[...]"); // TODO
+                    break;
                 default: break;
             }
         }
-        snprintf(invocation_str + written - 2, 256 - written, ")");
+        if (info->arg_count)
+            written -= 2; // trick to erease the extra commas
+        snprintf(str + written, MAX_LINE_SIZE - written, ")");
     }
     else
-        snprintf(invocation_str, 256, "unknown syscall (%ld)", syscall_id);
+        snprintf(str, 256, PLACEHOLDER_TEXT" (%ld)", syscall_id);
 
-    fprintf(stderr, "%-39s", invocation_str);
+    fprintf(stderr, "%-*s", MIN_PADDING - 1, str);
 }
 
 void output_return_value(long value, long syscall_id)
