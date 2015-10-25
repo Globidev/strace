4491544800e4c50137c888f969e15147d1fc8b1c
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-10-25 04:29:20 +0100

    More html parsing

--- syscall_gen/generator.py
+++ syscall_gen/generator.py
@@ -8,7 +8,18 @@ with urlopen(DATABASE_URL) as response:
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.find(class_='tbls-table')
     entries = table.find_all(class_='tbls-entry-collapsed')
+
     for entry in entries:
         defines = entry.find_all('td')
-        print(defines[1].string)
+        name = defines[1].string
+        args_row = entry.next_sibling
+        args = []
+        if 'tbls-arguments-collapsed' in args_row.attrs.get('class'):
+            types_row = args_row.find_all('tr')[1]
+            type_defines = types_row.find_all('td')
+            for define in type_defines:
+                type = define.find('strong')
+                args.append(type.string)
+
+        print('{%s, %s}' % (name, ', '.join(args)))
 
