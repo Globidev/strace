6ad90f64e3c6fd5ecd504d36a41ba7948e5664c9
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-11-02 03:23:28 +0100

    Refactored a lot of the logging logic :lipstick:

--- includes/log.h
+++ includes/log.h
@@ -1,6 +1,13 @@
 #pragma once
 
+#include <signal.h>
+
 #include "syscall.h"
+
+#define MAX_LINE_SIZE 4096
+
+void str_printf(char *str, int *written, const char *format, ...);
+void write_arg(void *arg, type arg_type, char *str, int *written);
 
 void output_invocation(long syscall_id, syscall_arg *args);
 void output_return_value(long value, long syscall_id);
@@ -8,7 +15,4 @@ void output_unknown_return_value();
 
 void output_signal(const siginfo_t *siginfo);
 
-#define EXIT_MESSAGE "+++ exited with %d +++\n"
-#define KILLED_MESSAGE "+++ Killed by %s +++\n"
-#define KILLED_AND_DUMPED_MESSAGE "+++ Killed by %s (core dumped) +++\n"
 void output_exit(int status, int exit_code);
--- srcs/log.c
+++ srcs/log.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <stdarg.h>
 #include <string.h>
 #include <errno.h>
-#include <signal.h>
 #include <sys/wait.h>
 
 #include "error.h"
@@ -12,53 +10,8 @@
 #include "signals.h"
 #include "errno_.h"
 
-#define MAX_LINE_SIZE 4096
 #define MIN_PADDING 40
 #define PLACEHOLDER_TEXT "Unknown syscall"
-
-static void str_write(char *str, int *written, const char *format, ...)
-{
-    va_list args;
-
-    va_start(args, format);
-    if (*written < MAX_LINE_SIZE) {
-        *written += vsnprintf(
-            str + *written,
-            MAX_LINE_SIZE - *written,
-            format,
-            args
-        );
-    }
-    va_end(args);
-}
-
-static void write_string(char *out, int *written, char *str)
-{
-    str_write(
-        out,
-        written,
-        "\"%s\"%s, ",
-        str, // TODO: escape
-        str[STRING_PEEK_MAX_SIZE - 1] && strlen(str) == STRING_PEEK_MAX_SIZE ? "..." : ""
-    );
-    free(str);
-}
-
-static void write_array(char *out, int *written, char **array)
-{
-    unsigned i;
-
-    str_write(out, written, "[");
-    if (*array) {
-        write_string(out, written, *array);
-        for (i = 1; array[i]; ++i) {
-            str_write(out, written, ", ");
-            write_string(out, written, array[i]);
-        }
-    }
-    str_write(out, written, "]");
-    free(array);
-}
 
 void output_invocation(long syscall_id, syscall_arg *args)
 {
@@ -70,39 +23,18 @@ void output_invocation(long syscall_id, syscall_arg *args)
     info = get_syscall_info(syscall_id);
     if (info) {
         written = snprintf(str, MAX_LINE_SIZE, "%s(", info->name);
-        for (i = 0; i < info->arg_count; ++i) {
-            switch (info->args_type[i]) {
-                case int_:
-                    str_write(str, &written, "%d, ", (int)(long)args[i]);
-                    break ;
-                case uint_:
-                    str_write(str, &written, "%u, ", (unsigned)(long)args[i]);
-                    break ;
-                case long_:
-                    str_write(str, &written, "%ld, ", (long)args[i]);
-                    break ;
-                case ulong_:
-                    str_write(str, &written, "%lu, ", (unsigned long)args[i]);
-                    break ;
-                case pointer_:
-                    str_write(str, &written, "%p, ", args[i]);
-                    break;
-                case string_:
-                    write_string(str, &written, args[i]);
-                    break;
-                case array_:
-                    write_array(str, &written, args[i]);
-                    break;
-                default: break;
+        if (info->arg_count) {
+            write_arg(args[0], info->args_type[0], str, &written);
+            for (i = 1; i < info->arg_count; ++i) {
+                str_printf(str, &written, ", ");
+                write_arg(args[i], info->args_type[i], str, &written);
             }
         }
         written = written > MAX_LINE_SIZE ? MAX_LINE_SIZE : written;
-        if (info->arg_count)
-            written -= 2; // trick to erease the extra commas
         snprintf(str + written, MAX_LINE_SIZE - written, ")");
     }
     else
-        snprintf(str, 256, PLACEHOLDER_TEXT" (%ld)", syscall_id);
+        snprintf(str, MAX_LINE_SIZE, PLACEHOLDER_TEXT" (%ld)", syscall_id);
 
     fprintf(stderr, "%-*s", MIN_PADDING - 1, str);
 }
@@ -131,12 +63,11 @@ void output_unknown_return_value()
     fprintf(stderr, " = ?\n");
 }
 
-#define SIGINFO_OUTPUT_FORMAT "--- %s {si_signo=%d, si_code=%d, si_pid=%d, si_uid=%d} ---\n"
 void output_signal(const siginfo_t *siginfo)
 {
     fprintf(
         stderr,
-        SIGINFO_OUTPUT_FORMAT,
+        "--- %s {si_signo=%d, si_code=%d, si_pid=%d, si_uid=%d} ---\n",
         SIGNAMES[siginfo->si_signo],
         siginfo->si_signo,
         siginfo->si_code,
@@ -144,6 +75,10 @@ void output_signal(const siginfo_t *siginfo)
         siginfo->si_uid
     );
 }
+
+#define EXIT_MESSAGE "+++ exited with %d +++\n"
+#define KILLED_MESSAGE "+++ Killed by %s +++\n"
+#define KILLED_AND_DUMPED_MESSAGE "+++ Killed by %s (core dumped) +++\n"
 
 void output_exit(int status, int exit_code)
 {
